27ecb8924c23f1df9fa17046c7d85362073b7b2f
Ekansh Saxena <ekansh005@gmail.com>
2023-11-08 19:33:55 +0200

    Add sandbox

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,10 +1,14 @@
 const cds = require("@sap/cds");
 
-module.exports = async function () {
+module.exports = cds.service.impl(async function () {
+  const bupa = await cds.connect.to("API_BUSINESS_PARTNER");
+  this.on("READ", "Suppliers", async (req) => {
+    return bupa.run(req.query);
+  });
   this.after("READ", "Risks", (risksData) => {
     const risks = Array.isArray(risksData) ? risksData : [risksData];
     risks.forEach((risk) => {
       risk.criticality = risk.impact >= 100000 ? 1 : 2;
     });
   });
-};
+});
